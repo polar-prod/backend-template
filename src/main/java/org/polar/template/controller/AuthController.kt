e979d4a9cd8f7c9351be23d41b0316a8400fff99
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-08-27 22:46:06 +0200

    added basic auth with third party services via supabase

--- src/main/java/org/polar/template/controller/AuthController.kt
+++ src/main/java/org/polar/template/controller/AuthController.kt
@@ -1,15 +1,36 @@
 package org.polar.template.controller
 
-import io.github.jan.supabase.SupabaseClient
+import kotlinx.coroutines.runBlocking
 import org.polar.template.service.AuthService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/api/auth")
-class AuthController(private val authService: AuthService) {
+class AuthController(
+    private val authService: AuthService
+) {
 
-    @Autowired
-    lateinit var supabase: SupabaseClient
+    @GetMapping("/google")
+    fun getGoogleUrl(@RequestParam redirect: String): String {
+        return runBlocking {
+            authService.googleAuthUrl(redirect)
+        }
+    }
+
+    @GetMapping("/github")
+    fun getGithubUrl(@RequestParam redirect: String): String {
+        return runBlocking {
+            authService.gitHubAuthUrl(redirect)
+        }
+    }
+
+    @GetMapping("/github")
+    fun getAppleUrl(@RequestParam redirect: String): String {
+        return runBlocking {
+            authService.appleAuthUrl(redirect)
+        }
+    }
 }
